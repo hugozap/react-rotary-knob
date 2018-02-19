c316a4517ea60045162094d647cb82ad6bf2896a
Hugo Zapata <hugozap@gmail.com>
2018-02-19 00:31:59 -0500

    mobile drag works, working on jump

--- src/index.js
+++ src/index.js
@@ -68,7 +68,27 @@ class Knob extends Component<KnobProps, KnobState> {
     onChange: function() {},
     skin: defaultSkin
   }
-  componentDidMount() {}
+  componentDidMount() {
+    
+
+    setTimeout(()=>{
+      this.setupDragging(d3.select(container))      
+    },0)
+  }
+
+  componentWillReceiveProps(nextProps) {
+    //should recalculate scale?
+    const {pmin, pmax} = this.props;
+    const {nmin, nmax} = nextProps;
+    if(pmin != nmin || pmax != nmax) {
+      this.scale = d3
+      .scaleLinear()
+      .domain([nmin, nmax])
+      .range([0, 359]);
+    }
+  }
+
+
 
   saveRef(elem: any) {
     if (!this.state.svgRef) {
@@ -76,43 +96,99 @@ class Knob extends Component<KnobProps, KnobState> {
     }
   }
 
+
+  saveParentRef(container) {
+    this.container = container;
+  }
+
+  onAngleChanged(angle) {
+    //Calculate domain value
+    console.log(angle)
+    let domainValue = this.scale.invert(angle);
+    this.props.onChange(domainValue);
+  };
+
+  setupDragging(elem: d3.Selection<any>) {
+    if ( elem == null) {
+      return;
+    }
+    // Add dragging behavior to selector element
+    var self = this;
+    const cx = 100;
+    const cy = 100;
+    let { value } = this.props;
+    const initialAngle = this.scale(value);
+    
+
+    function started() {
+      elem.classed("dragging", true);
+      d3.event.on("drag", dragged).on("end", ended);
+
+      //change will be relative to starting point for greater precision
+      const startPos = { x: d3.event.x - cx, y: d3.event.y - cy };
+      const startAngle = u.getAngleForPoint(startPos.x, startPos.y);
+      let lastPos = startPos;
+      let lastAngle = u.getAngleForPoint(lastPos.x, lastPos.y);
+
+      function dragged() {
+        console.log('d3.event', d3.event)
+        console.log('d3.touch', d3.touch(elem))
+        let currentPos = { x: d3.event.x - cx, y: d3.event.y - cy };
+        let currentAngle = u.getAngleForPoint(currentPos.x, currentPos.y);
+        const deltaAngle = currentAngle - startAngle;
+        lastPos = currentPos;
+        const finalAngle = (initialAngle + deltaAngle) % 360;
+        self.onAngleChanged(finalAngle);
+      }
+
+      function ended() {
+        elem.classed("dragging", false);
+      }
+    }
+
+    elem.call(d3.drag().on("start", started));
+  }
+
+  componentWillMount() {
+    this.scale = d3
+      .scaleLinear()
+      .domain([this.props.min, this.props.max])
+      .range([0, 359]);
+  }
+
+
   render() {
 
     const { value, min, max, onChange, skin, ...rest } = this.props;
     console.log(`render ${value}`)
-    
-    const scale = d3
-      .scaleLinear()
-      .domain([min, max])
-      .range([0, 359]);
-    const angle = scale(value);
+    const angle = this.scale(value);
 
-    const onAngleChange = angle => {
-      //Calculate domain value
-      console.log(angle)
-      let domainValue = scale.invert(angle);
-      this.props.onChange(domainValue);
-    };
+
 
     const onFormControlChange = newVal => {
       console.log('input control changed: ' +newVal);
       this.props.onChange(newVal);
     }
     return (
+      <React.Fragment>
+      <div ref={this.saveParentRef.bind(this)}>
       <Samy svgXML={skin.svg} onSVGReady={this.saveRef.bind(this)} {...rest}>
-        {this.state.svgRef && (
+        
+        <SvgProxy selector="#knob" transform={`$ORIGINAL rotate(${angle}, ${skin.knobX}, ${skin.knobY})`}/>
+        <SvgProxy selector="tspan">{value.toFixed(2)}</SvgProxy>
+      </Samy>
+      </div>
+      {this.state.svgRef && (
           <RotateView
             svg={this.state.svgRef}
             angle={angle}
             cx={100}
             cy={100}
-            onAngleChange={onAngleChange}
           />
         )}
-        <SvgProxy selector="#knob" transform={`$ORIGINAL rotate(${angle}, ${skin.knobX}, ${skin.knobY})`}/>
-        <SvgProxy selector="tspan">{value.toFixed(2)}</SvgProxy>
         <InternalInput value={value} min={min} max={max} onChange={onFormControlChange} />
-      </Samy>
+        
+      </React.Fragment>
     );
   }
 }
@@ -151,45 +227,8 @@ class RotateView extends Component<RotateViewProps> {
   renderControls(props: RotateViewProps) {
     const { r, cx, cy, angle } = props;
     const svgRef = d3.select(props.svg || ".main-svg");
-    let container = svgRef.select('#knob');
-    
-    this.setupDragging(container);
-  }
-
-  setupDragging(elem: d3.Selection<any>) {
-    if (elem == null) {
-      return;
-    }
-    // Add dragging behavior to selector element
-    var self = this;
-    let { cx, cy } = this.props;
-
-    function started() {
-      elem.classed("dragging", true);
-      d3.event.on("drag", dragged).on("end", ended);
-
-      //change will be relative to starting point for greater precision
-      const startPos = { x: d3.event.x - cx, y: d3.event.y - cy };
-      const initialAngleProp = self.props.angle;
-      const startAngle = u.getAngleForPoint(startPos.x, startPos.y);
-      let lastPos = startPos;
-      let lastAngle = u.getAngleForPoint(lastPos.x, lastPos.y);
-
-      function dragged() {
-        let currentPos = { x: d3.event.x - cx, y: d3.event.y - cy };
-        let currentAngle = u.getAngleForPoint(currentPos.x, currentPos.y);
-        const deltaAngle = currentAngle - startAngle;
-        lastPos = currentPos;
-        const finalAngle = (initialAngleProp + deltaAngle) % 360;
-        self.props.onAngleChange(finalAngle);
-      }
-
-      function ended() {
-        elem.classed("dragging", false);
-      }
-    }
-
-    elem.call(d3.drag().on("start", started));
+    let container = svgRef;
+    let knob = svgRef.select('#knob')
   }
 
   clearControls() {
